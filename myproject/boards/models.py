bc98ca6347bf27da1c12ec01253670e79fce14ed
preciousimo <preciousimoniakemu@gmail.com>
2021-05-17 16:15:18 -0700

    updated model

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -15,7 +15,7 @@ class Board(models.Model):
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
-    last_update = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
 
