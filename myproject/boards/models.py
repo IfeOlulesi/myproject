6849c0751c2076c6f402c17f28acd0257b2511b9
preciousimo <preciousimoniakemu@gmail.com>
2021-05-17 14:51:46 -0700

    create template and run test

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    is_superuser = models.BooleanField()
+
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_update = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
+    starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+
+class Post(models.Model):
+    message = models.TextField()
+    topics = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+
+    return render(request, 'home.html', {'boards': boards})
