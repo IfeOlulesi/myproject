69bafa71a636e5c396abba5fb546753a320ec93b
preciousimo <preciousimoniakemu@gmail.com>
2021-05-18 15:10:21 -0700

    create navigation links in the screens

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,8 +1,8 @@
 from django.urls import reverse
-from django.test import TestCase
 from django.urls import resolve
-from .views import home
-
+from django.test import TestCase
+from .views import home, board_topics
+from .models import Board
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
         url = reverse('home')
@@ -12,3 +12,20 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk'})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk':5})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
